410a3c91fc54b9cec0800451f3bbba6834d909ae
baumgartnerdataprojects <baumgartnerdataprojects@gmail.com>
2026-09-01 22:05:29 -0300

    Update project name and profile in dbt_project.yml for tag masking

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,23 +1,16 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
+    {#
+      Seeds must always land in RAW.
+      Non-seed models should use the configured custom schema when present,
+      otherwise fall back to the default target schema.
+    #}
 
-    {% set default_schema = target.schema %}
-
-    {# seeds go in a global `raw` schema #}
     {% if node.resource_type == 'seed' %}
+        {{ 'RAW' }}
+    {% elif custom_schema_name is not none and custom_schema_name | trim != '' %}
         {{ custom_schema_name | trim }}
-
-    {# non-specified schemas go to the default target schema #}
-    {% elif custom_schema_name is none %}
-        {{ default_schema }}
-
-
-    {# specified custom schema names go to the schema name prepended with the the default schema name in prod (as this is an example project we want the schemas clearly labeled) #}
-    {% elif target.name == 'prod' %}
-        {{ default_schema }}_{{ custom_schema_name | trim }}
-
-    {# specified custom schemas go to the default target schema for non-prod targets #}
     {% else %}
-        {{ default_schema }}
+        {{ target.schema }}
     {% endif %}
-
 {% endmacro %}
+
